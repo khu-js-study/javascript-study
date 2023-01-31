32e977a5be929df63536118bd2ea63c56173c8db
LSH-0125e <tpgur0125@khu.ac.kr>
2023-01-31 19:23:50 +0900

    faet(08): 08장 예제 코드 추가

---
+++ 2주차 (05~08장)/08. 제어문/LSH0125/example.js
@@ -0,0 +1,13 @@
+// break 문과 continue 문의 차이
+
+for (var i = 0; i <= 5; i++) {
+    console.log('break');
+    break;
+    console.log('unprinted');
+}  
+
+for (var i = 0; i <= 5; i++) {
+    console.log('continue');
+    continue;
+    console.log('unprinted');
+} 
