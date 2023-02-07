08a0bab296822ecf8d231414ca28bf8996fa58f6
DongHyun Lee <donghyuni98@gmail.com>
2023-02-07 20:05:40 +0900

    docs(06) 06. 데이터 타입 (#36)
    * docs(06): 06장 기억에 남는 내용 추가
    
    * docs(06): 06장 간단한 퀴즈 추가
    
    * docs(06): 06장 jsdoc 코드 예제 추가

---
+++ 2주차 (05~08장)/06. 데이터 타입/L2HYUNN/example.js
@@ -0,0 +1,11 @@
+/**
+ * @description 두 수의 합을 구한다.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ *
+ * @see {@link https://jsdoc.app/index.html }
+ * @author L2HYUNN <donghyuni98@gmail.com>
+ */
+const add = (a, b) => a + b;
